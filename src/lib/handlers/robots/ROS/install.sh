2ad4ae7305310dcca100ee54ca0a24843e816d53
Stephen Smith <smsmit18@ncsu.edu>
2012-08-03 14:49:20 -0400

    udated install script

--- src/lib/handlers/robots/ROS/install.sh
+++ src/lib/handlers/robots/ROS/install.sh
@@ -21,14 +21,14 @@ echo "source /opt/ros/fuerte/setup.bash" >> ~/.bashrc
 . ~/.bashrc
 source /opt/ros/furete/setup.bash
 sudo apt-get install python-rsvg
-PATHSM='/opt/ros/fuerte/stacks/simulator_gazebo/gazebo_worlds/'
+PATHSM='/opt/ros/fuerte/stacks/simulator_gazebo/gazebo_worlds'
 sudo cp ltlmop_map.world $PATHSM/worlds/ltlmop_map.world
 sudo chmod 777 $PATHSM/worlds/ltlmop_map.world
 sudo chmod 777 $PATHSM/worlds
 sudo chmod 777 $PATHSM/launch
 sudo cp ltlmop.launch $PATHSM/launch/ltlmop.launch
 sudo chmod 777 $PATHSM/launch/ltlmop.launch
-PATHLOC='/opt/ros/fuerte/stacks/simulator_gazebo/gazebo/gazebo/share/gazebo-1.0.2/Media/materials/'
+PATHLOC='/opt/ros/fuerte/stacks/simulator_gazebo/gazebo/gazebo/share/gazebo-1.0.2/Media/materials'
 sudo chmod 777 /opt/ros/fuerte/stacks/simulator_gazebo/gazebo/gazebo/share/gazebo-1.0.2/Media/materials/textures
 if ! grep -q ltlmop $PATHLOC/scripts/Gazebo.material 
 	then sudo cat ltlmop.material >> $PATHLOC/scripts/Gazebo.material 
