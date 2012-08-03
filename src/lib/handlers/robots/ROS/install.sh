8ff02a408da4d37bc729f4fd1dc21bdc62858260
Stephen Smith <smsmit18@ncsu.edu>
2012-08-03 14:12:52 -0400

    udated install script

--- src/lib/handlers/robots/ROS/install.sh
+++ src/lib/handlers/robots/ROS/install.sh
@@ -6,8 +6,7 @@ unzip Polygon-2.0.5.zip
 cd Polygon-2.0.5
 sudo python setup.py install
 cd ..
-sudo mkdir /etc/apt/sources.list.d/ros
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros/latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install ros-fuerte-desktop-full
