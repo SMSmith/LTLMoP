f3c5bf96c81aaef23edfd7361238f18bf1f5be12
Stephen Smith <smsmit18@ncsu.edu>
2012-08-03 13:29:33 -0400

    updates to install script

--- src/lib/handlers/robots/ROS/install.sh
+++ src/lib/handlers/robots/ROS/install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 echo Installing ROS...
+sudo apt-get install python-wxglade
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros/latest.list'
 wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 sudo apt-get update
